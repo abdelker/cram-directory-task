f69d88aee75aed53fab3fea98cbcbaa562735e08
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-20 16:54:02 +0100

    define new global variables

--- src/dt-init.lisp
+++ src/dt-init.lisp
@@ -1,15 +1,15 @@
 (in-package :dt)
 
-(defvar ctx-designate nil)
-(defvar table "table_1")
-(defvar table1 "table_1")
+(defvar *ctx-designate* nil)
+(defvar *robot-name* nil)
+(defvar table nil)
 (defvar str_sparql nil)
 
 (defvar cheat nil)
-(defvar robot-name nil)
+(defvar *robot-name* nil)
 (defvar lang nil)
 
-(defvar cubes nil)
+(defvar *cubes* nil)
 (defvar current-cube nil)
 (defvar nb-cubes nil)
 
@@ -20,19 +20,20 @@
 (defvar add-callback nil)
 
 (defun init-dt (robot-name lang)
-
-  (setf robot-name robot-name)
+  (setf *robot-name* robot-name)
   (setf table1 "table_1")
   (setf cheat nil)
 
   (onto::wait-init)
-  (onto::add-onto robot-name)
-  (onto::get-onto robot-name)
+  (onto::add-onto *robot-name*)
+  (onto::get-onto *robot-name*)
   (onto::add-onto "human_0")
   (onto::get-onto "human_0")
   (set-ontology)
 
-  (setf cubes nil)
+  (setf *ctx-designate* nil)
+
+  (setf *cubes* nil)
   (setf current-cube nil)
   (setf nb-cubes 0)
 
@@ -42,6 +43,8 @@
   (setf pick-callback nil)
   (setf add-callback nil)
 
-)
+  (init-ros-dt))
+
+
 
 
