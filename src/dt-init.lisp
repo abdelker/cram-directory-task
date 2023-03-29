78848f0cb5fb1871f3c842456d0d0141aed089eb
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-29 18:17:25 +0200

    add close onto func after get

--- src/dt-init.lisp
+++ src/dt-init.lisp
@@ -1,6 +1,6 @@
 (in-package :dt)
 
-(defvar *ctx-designate* nil)
+;;(defvar *ctx-designate* nil)
 (defvar *robot-name* nil)
 (defvar table nil)
 (defvar str_sparql nil)
@@ -20,30 +20,38 @@
 (defvar add-callback nil)
 
 (defun init-dt (robot-name lang)
+  
+ ;; (start-ros-node "cram_dt")
+  (init-ros-dt)
   (setf *robot-name* robot-name)
-  (setf table1 "table_1")
+  (setf table1 "table_lack")
   (setf cheat nil)
 
+  (onto::init-ontologies-man)
+  ;;(onto::init-manager)
   (onto::wait-init)
   (onto::add-onto *robot-name*)
   (onto::get-onto *robot-name*)
+  (onto::close-onto)
   (onto::add-onto "human_0")
   (onto::get-onto "human_0")
-  (set-ontology)
+  (onto::close-onto)
+;   (set-ontology)
 
-  (setf *ctx-designate* nil)
+;   (setf *ctx-designate* nil)
 
-  (setf *cubes* nil)
-  (setf current-cube nil)
-  (setf nb-cubes 0)
+;   (setf *cubes* nil)
+;   (setf current-cube nil)
+;   (setf nb-cubes 0)
 
-  (setf id-mementar-callback -1)
-  (setf id-mementar-add-callback -1)
+;   (setf id-mementar-callback -1)
+;   (setf id-mementar-add-callback -1)
 
-  (setf pick-callback nil)
-  (setf add-callback nil)
+;   (setf pick-callback nil)
+;   (setf add-callback nil)
 
-  (init-ros-dt))
+  )
+  
 
 
 
