09d5854a1554873e5380685437b19d10c6ed81b1
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-17 14:35:52 +0200

    add lisp file for constructing agent-desig

---
+++ src/agent-knowledge.lisp
@@ -0,0 +1,11 @@
+(defvar *agent1-desig* nil)
+
+
+(defun define-agent-desig (agent-name)
+ (let ((agent-role (svref (onto::get-on agent-name "hasRole" agent-name) 0)))
+  (let ((?agent-name agent-name) (?agent-role agent-role))
+   (setf *agent1-desig*
+    (desig:an agent (type :human) 
+                    (:name ?agent-name) 
+                    (:role ?agent-role))))))
+                         
