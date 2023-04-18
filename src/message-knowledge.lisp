c217e4e038b0c9557b4348ab8e0150251efb9542
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-18 16:31:40 +0200

    add reply and state

--- src/message-knowledge.lisp
+++ src/message-knowledge.lisp
@@ -5,12 +5,15 @@
 (defun construct-message-desig (sentences)
  (let ((u-resp (understand-call sentences)))
   (let ((query (nth 0 u-resp)) (?communicated-action (nth 1 u-resp)))
-   (let ((?communicated-object (nth 2 (designate-object query))))
-    (cond ((not (string= ?communicated-action ""))
-           (setf *request-msg-desig* (copy-designator *request-msg-desig*
-                                      :new-description `((:communicated-action ,?communicated-action))))))
-    (cond ((not (eql ?communicated-object nil))      
-           (setf *request-msg-desig* (copy-designator *request-msg-desig*
-                                      :new-description `((:communicated-object ,?communicated-object)))))))))
- (values *request-msg-desig*))
+   (let ((desig-results (designate-object query))) 
+    (let ((?communicated-object (nth 2 desig-results))
+          (reply-msg (nth 3 desig-results))
+          (state (nth 0 desig-results)))
+     (cond ((not (string= ?communicated-action ""))
+            (setf *request-msg-desig* (copy-designator *request-msg-desig*
+                                       :new-description `((:communicated-action ,?communicated-action))))))
+     (cond ((not (eql ?communicated-object nil))      
+            (setf *request-msg-desig* (copy-designator *request-msg-desig*
+                                       :new-description `((:communicated-object ,?communicated-object))))))
+     (multiple-value-list (values *request-msg-desig* reply-msg state)))))))
                      
