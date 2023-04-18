4883d9ae5d26f33d923a3c6124f59bf5599ba453
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-18 14:01:19 +0200

    msg desig is copied when newly created

--- src/message-knowledge.lisp
+++ src/message-knowledge.lisp
@@ -1,13 +1,16 @@
 (in-package :dt)
 
-(defvar *msg-desig* nil)
+(defparameter *request-msg-desig* (desig:a message (type :request)))
 
 (defun define-message-desig (sentences)
  (let ((u-resp (understand-call sentences)))
   (let ((query (nth 0 u-resp)) (?communicated-action (nth 1 u-resp)))
    (let ((?communicated-object (nth 2 (designate-object query))))
-    (setf *msg-desig*
-     (desig:a message (type :request) 
-                     (:communicated-action ?communicated-action) 
-                     (:communicated-object ?communicated-object)))))))
-
+    (cond ((not (string= ?communicated-action ""))
+           (setf *request-msg-desig* (copy-designator *request-msg-desig*
+                                      :new-description `((:communicated-action ,?communicated-action))))))
+    (cond ((not (eql ?communicated-object nil))      
+           (setf *request-msg-desig* (copy-designator *request-msg-desig*
+                                      :new-description `((:communicated-object ,?communicated-object)))))))))
+ (values *request-msg-desig*))
+                     
