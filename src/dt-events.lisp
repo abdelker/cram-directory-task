785094edfb0860bd19e86c4b13dfe8cec00775a9
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-21 11:47:02 +0200

    add different event classes for dt

--- src/dt-events.lisp
+++ src/dt-events.lisp
@@ -1,1 +1,13 @@
 (in-package :dt)
+
+(defclass waiting-for-human1 (event)
+)
+
+(defclass looking-at-cube1 (event)
+)
+
+(defclass waiting-for-human2 (event)
+)
+
+(defclass looking-at-cube2 (event)
+)
