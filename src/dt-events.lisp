c366c8ebba69282e2bf194f91c0cc440ffb5ce92
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-21 14:21:12 +0200

    add missing pkg specifier

--- src/dt-events.lisp
+++ src/dt-events.lisp
@@ -2,97 +2,96 @@
 
 (defparameter *count* 0)
 (defparameter *mvt-duration* 3)
-(defparamter *agent-state* "in-progress")
+(defparameter *agent-state* "in-progress")
 
+                  
 ;;demo
-
-
-
 
 (defun init-dt-demo ()
 
 ;;set goal/event to WaitingHuman1
 
- (on-event (make-instance 'waiting-for-human1))
- (roslisp:loop-at-most-every 1  (on-event (make-instance 'monitor))))
+ (occasions-events:on-event (make-instance 'waiting-for-human1))
+ (roslisp:loop-at-most-every 1  (occasions-events:on-event (make-instance 'monitor))))
 
 
 (defun reset-goal ()
 
 ;;set goal/event to WaitingHuman1
- (on-event (make-instance 'waiting-for-human1)))
+ (occasions-events:on-event (make-instance 'waiting-for-human1)))
 
 
 ;;event classes for goals
 
-(defclass monitor (event) ())
+(defclass monitor (occasions-events:event) ())
 
 
-(defclass waiting-for-human1 (event) ())
+(defclass waiting-for-human1 (occasions-events:event) ())
 
 
-; (defclass looking-at-cube1 (event))
+; (defclass looking-at-cube1 (occasions-events:event))
 
 
-; (defclass waiting-for-human2 (event))
+; (defclass waiting-for-human2 (occasions-events:event))
 
 
-; (defclass looking-at-cube2 (event))
+; (defclass looking-at-cube2 (occasions-events:event))
 
 ; ;; event classes for cube takes
 
-; (defclass wrong-cube-take (event)
+; (defclass wrong-cube-take (occasions-events:event)
 ;  ((no-cube-taken
 ;    :init-arg :no-take :reader no-take
 ;    :init-form nil)))
 
 
-; (defclass good-cube-take (event))
+; (defclass good-cube-take (occasions-events:event))
 
 
 ;;event-handlers
 
-(defmethod on-event ((event monitor))
+(defmethod occasions-events:on-event ((occasions-events:event monitor))
  (write-line "I am monitoring")
  (cond ((string= *agent-state* "in-progress")
-        (setf *count* (+ count 1)))
+        (setf *count* (+ *count* 1)))
        ((reset-goal))))
 
 
-(defmethod on-event ((event waiting-for-human1))
+(defmethod occasions-events:on-event ((occasions-events:event waiting-for-human1))
+ (write-line "I am waiting")
  (cond ((>= *count* (+ *mvt-duration* 2))
         (progn 
                (setf *count* 0)
 ;;(perform lookobject)
-         (princ "waiting for human")))))
-        ;; (on-event (make-instance 'looking-at-cube1))))))
+         (princ "still waiting for human")))))
+        ;; (occasions-events:on-event (make-instance 'looking-at-cube1))))))
 
 
-; (defmethod on-event ((event looking-at-cube1))
+; (defmethod occasions-events:on-event ((occasions-events:event looking-at-cube1))
 ;  (setf *count* 0)
 ; ;;(perform lookobject)
-;  (on-event (make-instance 'waiting-for-human2)))
+;  (occasions-events:on-event (make-instance 'waiting-for-human2)))
 
 
-; (defmethod on-event ((event waiting-for-human2))
+; (defmethod occasions-events:on-event ((occasions-events:event waiting-for-human2))
 ;  (cond 
 ;   ((eql *count* *mvt-duration*))
 ;    ;(perform sayWaiting))
    
 ;   ((>= *count* (+ *mvt-duration* 2))
 ;   ;; (perform lookObject)
-;    (on-event (make-instance 'looking-at-cube2)))))
+;    (occasions-events:on-event (make-instance 'looking-at-cube2)))))
 
 
-; (defmethod on-event ((event looking-at-cube2))
+; (defmethod occasions-events:on-event ((occasions-events:event looking-at-cube2))
 ;  (setf *count* 0)
 ; ;;(perform lookobject)
-;  (on-event (make-instance 'wrong-cube-take)))
+;  (occasions-events:on-event (make-instance 'wrong-cube-take)))
 
 
 ; ;; event-handlers for cube takes
 
-; (defmethod on-event ((event wrong-cube-take))
+; (defmethod occasions-events:on-event ((occasions-events:event wrong-cube-take))
 ; ;;(perform lookAt)
 ;  (cond 
 ;   ((eql (no-take event) t))))
@@ -105,4 +104,4 @@
 ;   ;; (perform lookExperimentator)
 ;   ;; (perform pointAt)
 ; ;;   (perform sayhelp)
-;   ;; (on-event (make-instance 'looking-at-cube2)))))
+;   ;; (occasions-events:on-event (make-instance 'looking-at-cube2)))))
